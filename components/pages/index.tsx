664f6ab474bccd1424c035e2479e8657e9c7b5a6
kizahasi <rasis.aneki@gmail.com>
2021-12-24 23:57:57 +0900

    feat: index.tsxを少し改善

--- components/pages/index.tsx
+++ components/pages/index.tsx
@@ -2,5 +2,10 @@ import { NextPage } from 'next';
 import Link from 'next/link';
 
 export const Index: NextPage = () => {
-    return <Link href="/web-server">Webサーバーの設定を作成する</Link>;
+    return (
+        <div style={{ padding: 16 }}>
+            <h2>Floconツール</h2>
+            <Link href="/web-server">Webサーバーの設定を作成する</Link>
+        </div>
+    );
 };
