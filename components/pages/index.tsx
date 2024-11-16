6f44d09a21202fb0a0502421a15bd73e84a0d1fd
kizahasi <21971954+kizahasi@users.noreply.github.com>
2024-11-16 22:04:14 +0900

    flocon.appへ移行。packageManagerを設定

--- components/pages/index.tsx
+++ components/pages/index.tsx
@@ -1,24 +1,9 @@
 import { NextPage } from 'next';
-import Link from 'next/link';
 
 export const Index: NextPage = () => {
     return (
         <div style={{ padding: 16 }}>
-            <h2>Floconツール</h2>
-            <p>
-                <Link href="/web-server">Webサーバーの設定を作成する</Link>
-            </p>
-            <p>
-                <Link href="/bcrypt">エントリーパスワードに用いるbcryptハッシュを生成する</Link>
-            </p>
-            <h2>その他</h2>
-            <a
-                href="https://github.com/flocon-trpg/tools"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                ソースコード
-            </a>
+            <a href="https://flocon.app/tools">https://flocon.app/tools</a> に移行しました。
         </div>
     );
 };
